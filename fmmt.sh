c1a7d0cd9a11b425127ec66cca8f627b5597805c
fbglv <fabio.galvagni@brixiaone.com>
2022-01-29 13:56:39 +0100

    Reorganized the first draft in a functional way.

--- fmmt.sh
+++ fmmt.sh
@@ -12,7 +12,7 @@ COL_DFT="\033[0m"
 usage()
 {
 	# [ ! -z "$ARG" ] && echo "Unknown option: "$COL_ERR"${ARG%%=*}"$COL_DFT
-	echo "Usage: "$COL_WRN"photoset [--tgt_dir=|-tg]=<target_directory> [--prefix=|-p]=<prefix> [--list-only|-lo] \"."$COL_DFT
+	echo "Usage: "$COL_WRN"fmmt [--tgt_dir=|-tg]=<target_directory> [--prefix=|-p]=<prefix> [--list-only|-lo] \"."$COL_DFT
 	echo "Bye!"
 	exit 1
 }
@@ -24,56 +24,68 @@ usage_unknown_param()
 }
 
 
-ARGS=$@
-[ -z "$ARGS" ] && usage
-for ARG in "$@"
-do
-# echo "ARG: $ARG"
-case $ARG in
-    -lo|--list-only)
-	LIST_ONLY="y"
-	;;
 
-    -td=*|--tgt_dir=*)
-	DIR_TGT="${ARG#*=}"
-	[ -z "$DIR_TGT" ] && usage
-    ;;
+main()
+{
+    ARGS=$@
+    [ -z "$ARGS" ] && usage
+    for ARG in "$@"
+    do
+    # echo "ARG: $ARG"
+    case $ARG in
+        -lo|--list-only)
+        LIST_ONLY="y"
+        ;;
 
-    -p=*|--prefix=*)
-	PREFIX="${ARG#*=}"
-    [ -z "$PREFIX" ] && usage
-	;;
-    
-	*)
-	echo "error: ${ARG#*=}"
-	usage_unknown_param
-	;;
-esac
-done
+        -td=*|--tgt_dir=*)
+        DIR_TGT="${ARG#*=}"
+        [ -z "$DIR_TGT" ] && usage
+        ;;
+
+        -p=*|--prefix=*)
+        PREFIX="${ARG#*=}"
+        [ -z "$PREFIX" ] && usage
+        ;;
+        
+        *)
+        echo "error: ${ARG#*=}"
+        usage_unknown_param
+        ;;
+    esac
+    done
+}
 
 
-echo "DIR_TGT: "$DIR_TGT # DEBUG
-[ ! -z "$DIR_TGT" ] && cd $DIR_TGT
+boh()
+{
+    echo "DIR_TGT: "$DIR_TGT # DEBUG
+    [ ! -z "$DIR_TGT" ] && cd $DIR_TGT
 
 
-# for FL in $(find . -type f \( -name "*.jpg" -o -name "*.jpeg" \))
-for FL in $(find . -type f \( -name "*.jpg" -o -name "*.jpeg" \) | grep -v $PREFIX | sort | uniq) # command substitution
-do
-    FL_BASE=$(basename -- "$FL")
-    echo "- "$FL_BASE
+    # for FL in $(find . -type f \( -name "*.jpg" -o -name "*.jpeg" \))
+    for FL in $(find . -type f \( -name "*.jpg" -o -name "*.jpeg" \) | grep -v $PREFIX | sort | uniq) # command substitution
+    do
+        FL_BASE=$(basename -- "$FL")
+        echo "- "$FL_BASE
 
-    GPSDATE=$(exif --tag=0x001d $FL_BASE | grep Value | sed -r 's/ Value\://' | sed 's/^ *//g' | sed -r 's/\:/\-/g' 1>/dev/null 2>&1) # removes the "Value :" substrings, trims the line, and changes the date format from YYYY:MM:DD to YYYY-MM-DD
-    
-    echo "GPSDATE: "$GPSDATE
-    
-    GPSDATE_YEAR=$(echo $GPSDATE | awk -F- '{print $1}')
-    GPSDATE_MONTH=$(echo $GPSDATE | awk -F- '{print $2}')
-    GPSDATE_DAY=$(echo $GPSDATE | awk -F- '{print $3}')
+        GPSDATE=$(exif --tag=0x001d $FL_BASE | grep Value | sed -r 's/ Value\://' | sed 's/^ *//g' | sed -r 's/\:/\-/g' 1>/dev/null 2>&1) # removes the "Value :" substrings, trims the line, and changes the date format from YYYY:MM:DD to YYYY-MM-DD
+        
+        echo "GPSDATE: "$GPSDATE
+        
+        GPSDATE_YEAR=$(echo $GPSDATE | awk -F- '{print $1}')
+        GPSDATE_MONTH=$(echo $GPSDATE | awk -F- '{print $2}')
+        GPSDATE_DAY=$(echo $GPSDATE | awk -F- '{print $3}')
 
-    FL_BASE_NEW=$PREFIX"_"$GPSDATE_YEAR"-"$GPSDATE_MONTH"-"$GPSDATE_DAY".jpg"
-    echo "new filename: $FL_BASE_NEW"
+        FL_BASE_NEW=$PREFIX"_"$GPSDATE_YEAR"-"$GPSDATE_MONTH"-"$GPSDATE_DAY".jpg"
+        echo "new filename: $FL_BASE_NEW"
 
-    # check if the date is already in use! (check if new filename is already been used)
+        # check if the date is already in use! (check if new filename is already been used)
 
-    ## do verbose
-done
+        ## do verbose
+    done
+}
+
+
+
+
+main 
