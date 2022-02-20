bd49ceb6012476ad82e9ea222b0bdcabcb184b13
fbglv <fabio.galvagni@brixiaone.com>
2022-02-20 20:39:31 +0100

    fmmt.sh arguments parsing adapted for zsh.

--- fmmt.sh
+++ fmmt.sh
@@ -1,14 +1,19 @@
-#!/bin/bash
+#!/bin/zsh
 
 #	requires exif in order to work
 
-# cd "/mnt/c/Users/FabioGalvagni/Google Drive/The Twins/Temp/"
+
+
 
 COL_ERR="\033[0;31m"
 COL_WRN="\033[0;33m"
 COL_DFT="\033[0m"
 
 
+
+#
+#   Informs the user about how to call the script
+#
 usage()
 {
 	# [ ! -z "$ARG" ] && echo "Unknown option: "$COL_ERR"${ARG%%=*}"$COL_DFT
@@ -19,12 +24,14 @@ usage()
 
 
 
-
+#
+#   Parses the input arguments
+#
 read_args()
 {
-    # ARGS=$@
     [ -z "$ARGS" ] && usage
-    for ARG in $ARGS #"$@"
+
+    for ARG in $ARGS
     do
         case $ARG in
             -d|--debug)
@@ -59,7 +66,9 @@ read_args()
 
 
 
-
+#
+#   Once the input arguments have been parsed, their combination validity is checked
+#
 check_args()
 {
     if [ ! -z $DEBUG ]; then
@@ -70,7 +79,10 @@ check_args()
         echo " - All: "$ARGS
     fi
 
-    if [ -z $DIR_SRC ] [ ! -z $DEBUG ] && ; then
+    #
+    #   Use case 01: only list the input files without performing any change 
+    #   
+    if [ -z $DIR_SRC ] && [ ! -z $CHECK_ONLY ] ; then
         echo "ERROR - "$COL_ERR"DIR_SRC"$COL_DFT" cannot be null!"
         usage
     fi
@@ -81,47 +93,19 @@ check_args()
 
 
 
-boh()
+main()
 {
-    echo "DIR_SRC: "$DIR_SRC # DEBUG
     [ ! -z "$DIR_SRC" ] && cd $DIR_SRC
-
-
-    echo "DIR_TGT: "$DIR_TGT # DEBUG
     [ ! -z "$DIR_TGT" ] && cd $DIR_TGT
 
-
-    pwd
-
-    # for FL in $(find . -type f \( -name "*.jpg" -o -name "*.jpeg" \))
-    for FL in $(find . -type f \( -name "*.jpg" -o -name "*.jpeg" \) | sort | uniq) # command substitution
-    # for FL in $(find . -type f \( -name "*.jpg" -o -name "*.jpeg" \) | grep -v $PREFIX | sort | uniq) # command substitution
-    do
-        FL_BASE=$(basename -- "$FL")
-        echo "- "$FL_BASE
-
-        GPSDATE=$(exif --tag=0x001d $FL_BASE | grep Value | sed -r 's/ Value\://' | sed 's/^ *//g' | sed -r 's/\:/\-/g' 1>/dev/null 2>&1) # removes the "Value :" substrings, trims the line, and changes the date format from YYYY:MM:DD to YYYY-MM-DD
-        
-        echo "GPSDATE: "$GPSDATE
-        
-        GPSDATE_YEAR=$(echo $GPSDATE | awk -F- '{print $1}')
-        GPSDATE_MONTH=$(echo $GPSDATE | awk -F- '{print $2}')
-        GPSDATE_DAY=$(echo $GPSDATE | awk -F- '{print $3}')
-
-        FL_BASE_NEW=$PREFIX"_"$GPSDATE_YEAR"-"$GPSDATE_MONTH"-"$GPSDATE_DAY".jpg"
-        echo "new filename: $FL_BASE_NEW"
-
-        # check if the date is already in use! (check if new filename is already been used)
-
-        ## do verbose
-    done
+    # pwd
 }
 
 
 
 
-ARGS=$@
+ARGS=($@)
 read_args
 check_args
+main
 
-# boh
